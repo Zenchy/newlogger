34c65d9ddc9600816afbd7c4481b731a89f25780
zenchy <ironhackermadrid@gmail.com>
2021-05-19 03:35:23 +0200

    views included

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 //Initialization
 const app =  express()
 
 //Settings
 app.set(`port`, process.env.PORT || 8000)
+app.set('views', path.join(__dirname, `/views`))
 
 module.exports = app
