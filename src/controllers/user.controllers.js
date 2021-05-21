6fb553bd358019d732ddc1f976f9c6fe06f1cd60
zenchy <ironhackermadrid@gmail.com>
2021-05-21 23:50:31 +0200

    Crud completo

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -6,24 +6,30 @@ UserControl.getUsers = async(req, res)=> {
   res.json(users)
 }
 
-UserControl.postUser = (req, res)=> {
+UserControl.postUser = async(req, res)=> {
   const newUser =  new User({
-    name: req.body.name
+    name: req.body.name,
+    emails: req.body.emails,
+    password: req.body.password,
+    cargo: req.body.cargo,
   })
-  newUser.save();
+  await newUser.save();
   res.json({ status: "User created" });
   }
 
-UserControl.getUser = (req, res)=>{
-  res.send('get User')
+UserControl.getUser = async(req, res)=>{
+  const userfound = await User.findById(req.params.id)
+  res.json(userfound)
 }
 
-UserControl.putUser = (req, res)=> {
+UserControl.putUser = async(req, res)=> {
+  await User.findByIdAndUpdate(req.params.id, req.body)
   res.send('Put user')
 }
 
-UserControl.deleteUser = (req, res)=>{
-  res.send('delete user')
+UserControl.deleteUser = async(req, res)=>{
+  await User.deleteOne({"_id": req.params.id})
+  res.json({status: "Deeted element"})
 }
 
 module.exports = UserControl
