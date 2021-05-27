b901ad1e0b1dac02fb43627aca32195a42f1ded0
zenchy <ironhackermadrid@gmail.com>
2021-05-28 00:48:47 +0200

    config cors

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ const cors = require ('cors')
 const app =  express()
 
 //Settings
+var corsOptions = {
+  origin:  "http://localhost:4200",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 app.set(`port`, process.env.PORT || 8000)
 app.set('views', path.join(__dirname, `views`))
 
@@ -21,7 +25,7 @@ app.use(cors())//cors({origin:"url_que_quiero"}) y encontes permite comunicacion
 //Global variables
 
 //Routes
-app.use('/api/',cors(), routes)
+app.use('/api/', cors(corsOptions), routes)
 
 //Static files
 app.use(express.static(path.join(__dirname, 'public')))
